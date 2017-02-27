ecd90764e565fc1e7fe8b55f0605faf1d917e717
Norman Maurer <norman_maurer@apple.com>
2017-02-27 21:09:07 +0100

    Metrics exposed by PooledByteBufAllocator needs to be correctly synchronized
    Motivation:
    
    As we may access the metrics exposed of PooledByteBufAllocator from another thread then the allocations happen we need to ensure we synchronize on the PoolArena to ensure correct visibility.
    
    Modifications:
    
    Synchronize on the PoolArena to ensure correct visibility.
    
    Result:
    
    Fix multi-thread issues on the metrics

--- buffer/src/main/java/io/netty/buffer/PoolSubpage.java
+++ buffer/src/main/java/io/netty/buffer/PoolSubpage.java
@@ -200,27 +200,35 @@ final class PoolSubpage<T> implements PoolSubpageMetric {
 
     @Override
     public String toString() {
-        if (!doNotDestroy) {
-            return "(" + memoryMapIdx + ": not in use)";
-        }
+        synchronized (chunk.arena) {
+            if (!doNotDestroy) {
+                return "(" + memoryMapIdx + ": not in use)";
+            }
 
-        return String.valueOf('(') + memoryMapIdx + ": " + (maxNumElems - numAvail) + '/' + maxNumElems +
-               ", offset: " + runOffset + ", length: " + pageSize + ", elemSize: " + elemSize + ')';
+            return String.valueOf('(') + memoryMapIdx + ": " + (maxNumElems - numAvail) + '/' + maxNumElems +
+                    ", offset: " + runOffset + ", length: " + pageSize + ", elemSize: " + elemSize + ')';
+        }
     }
 
     @Override
     public int maxNumElements() {
-        return maxNumElems;
+        synchronized (chunk.arena) {
+            return maxNumElems;
+        }
     }
 
     @Override
     public int numAvailable() {
-        return numAvail;
+        synchronized (chunk.arena) {
+            return numAvail;
+        }
     }
 
     @Override
     public int elementSize() {
-        return elemSize;
+        synchronized (chunk.arena) {
+            return elemSize;
+        }
     }
 
     @Override
